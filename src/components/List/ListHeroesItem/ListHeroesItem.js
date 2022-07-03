b9c1d7361ae5dbf40bc3b4b06f46a4905e10a1dc
Yaroslam <47461457+Yaroslam@users.noreply.github.com>
2022-07-03 17:30:20 +0300

    some work

--- src/components/List/ListHeroesItem/ListHeroesItem.js
+++ src/components/List/ListHeroesItem/ListHeroesItem.js
@@ -4,17 +4,32 @@ import Typography from '@mui/material/Typography';
 import { Box } from '@mui/system';
 
 import classes from './ListHeroesItem.module.scss'
+import {deleteHero} from "../../../store/actions/heroActions";
+import {useDispatch} from "react-redux";
+import {IconButton} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 
 const ListHeroesItem = (props) => {
 
+    const dispatch = useDispatch()
+
     const { owner, hero_name } = props.item
+
+    const submitDelete = () => {
+        dispatch(deleteHero(props.item.id))
+    }
 
     return (
         <Box className={classes.container}>
             <Box className={classes.item}>
                 <Typography sx={{ my: '10px' }} variant="string">Владелец: {owner}</Typography>
                 <Typography sx={{ my: '10px' }} variant="string">Имя героя: {hero_name}</Typography>
+            </Box>
+            <Box className={classes.buttons}>
+                <IconButton onClick={submitDelete}>
+                    <DeleteIcon />
+                </IconButton>
             </Box>
         </Box>
     );
--- src/store/actions/heroActions.js
+++ src/store/actions/heroActions.js
@@ -1,8 +1,14 @@
 import axios from 'axios'
 import { heroSlice } from '../reducers/heroSlice'
 import { api_key } from '../../config';
+import {itemSlice} from "../reducers/itemSlice";
 
 export const getHeroes = () => async dispatch => {
     await axios.get(`${api_key}/api/Heroes/`)
         .then((res) => dispatch(heroSlice.actions.setHeroes(res.data)))
+}
+
+export const deleteHero = (id) => async (dispatch) => {
+    await axios.delete(`${api_key}/api/Heroes/${id}/`)
+        .then(res => dispatch(heroSlice.actions.deleteHero(id)))
 }
